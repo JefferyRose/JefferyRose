4bc6e833d52b66cff91143f3b763140b19ddd966
JefferyRose <92181869+JefferyRose@users.noreply.github.com>
2021-10-08 14:53:50 -0500

    Add files via upload

---
+++ Magic 8 ball1.py
@@ -0,0 +1,26 @@
+import random as rd
+
+name = input("What is your name? ")
+print("Hello " + name)
+question = input("What is your question? (In yes or no format) ")
+answer = ""
+random_number = rd.randint(1, 9)
+
+if random_number == 1:
+    print("Yes - definitely!")
+elif random_number == 2:
+    print("It is decidedly so.")
+elif random_number == 3:
+    print("Without a doubt.")
+elif random_number == 4:
+    print("Reply hazy, try again.")
+elif random_number == 5:
+    print("Ask again later.")
+elif random_number == 6:
+    print("Better not tell you now.")
+elif random_number == 7:
+    print("My sources say no.")
+elif random_number == 8:
+    print("Outlook not so good.")
+elif random_number == 9:
+    print("very doubtful.")
